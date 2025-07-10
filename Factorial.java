9637e07aca8f555f2dcb7c9a6a4a24331785eeee
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-10 23:16:23 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,29 @@
+// Java Program to calculate factorial of a number using function
+
+import java.util.Scanner;
+
+public class FactorialProgram {
+
+    // Function to calculate factorial
+    static long factorial(int n) {
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the number
+        System.out.print("Enter a number to find its factorial: ");
+        int number = sc.nextInt();
+
+        // Call the function and print the result
+        long result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+
+        sc.close();
+    }
+}
