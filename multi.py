84d50e9c6238326c7c8245dac078abe353dd61bd
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-17 21:46:55 +0530

    Create multi.py

---
+++ multi.py
@@ -0,0 +1,27 @@
+// Multilevel Inheritance
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle starts");
+    }
+}
+
+class Car extends Vehicle {
+    void drive() {
+        System.out.println("Car drives");
+    }
+}
+
+class SportsCar extends Car {
+    void speed() {
+        System.out.println("Sports car speeds fast");
+    }
+}
+
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        SportsCar sc = new SportsCar();
+        sc.start(); // From Vehicle
+        sc.drive(); // From Car
+        sc.speed(); // From SportsCar
+    }
+}
