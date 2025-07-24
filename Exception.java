01a473f4ba758793a903daa9347d5d31ddc61d87
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-24 22:54:26 +0530

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,15 @@
+# Exception Handling
+public class DivideExample {
+    public static void main(String[] args) {
+        try {
+            int a = 10, b = 0;
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        }
+    }
+}
+
+output : Cannot divide by zero!
+
