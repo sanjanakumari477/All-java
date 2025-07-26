c7d08172d20580b191e5e8a0880aa87b0bd7348e
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-26 23:52:53 +0530

    Create AlbhaCheck.java

---
+++ AlbhaCheck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+	class Alphabet{
+        public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);	
+	System.out.print("enter any character=");
+	
+	char ch= sc.next().charAt(0);
+	if(ch>='A'&& ch<='Z')
+             {
+	    System.out.println("It is Uppercase");
+             }else 
+            {
+           System.out.println("It is Lowecase");
+
+            }
+      }
+ }
