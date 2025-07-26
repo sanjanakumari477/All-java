6614acc21d5474a1a379000b17aa820ee0c037a6
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-27 01:47:39 +0530

    Create CheckLeap.java

---
+++ CheckLeap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class LeapYear{
+ 
+public static void main(String[]sk)
+{
+System.out.print("Enter Any Year:");
+Scanner Year=new Scanner(System.in);
+int Y= Year.nextInt();
+ 
+	if((Y%4==0 && Y%100!=0 || Y%400==0))
+	{
+	System.out.print("This year is leap year.");
+	}
+	else{
+	System.out.print("This year is not leap year.");
+	}
+    }
+}
