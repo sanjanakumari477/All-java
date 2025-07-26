0e080be92eb895fe8a357353a0f46b00dd53adf3
Sanjana Kumari <sksanjana1502@gmail.com>
2025-07-27 00:26:01 +0530

    Create checkVowelConso.java

---
+++ checkVowelConso.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+   class VowelConsonent{
+      public static void main (String[]args)
+       {
+	Scanner S=new Scanner(System.in);
+        System.out.print("enter any character = ");
+        char ch= S.next().charAt(0);
+          
+ 	if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+     	   	{
+            System.out.println("This character is vowel");
+                  }
+             
+              else 
+           {
+            System.out.println("This character is consonent");
+                   }
+                }
+          }
