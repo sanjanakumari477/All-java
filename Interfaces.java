bc468110addfcb6ee411e54fec9de00099205813
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-07 23:37:58 +0530

    Create Interfaces.java

---
+++ Interfaces.java
@@ -0,0 +1,36 @@
+public interface Shape {
+    double getArea(); // abstract method
+}
+public class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5.0);
+        Shape rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Area of Circle: " + circle.getArea());
+        System.out.println("Area of Rectangle: " + rectangle.getArea());
+    }
+}
