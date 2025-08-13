efd75445cb2874059609f6877414745c9f3ac606
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-13 11:19:09 +0530

    Create ClassLoader.java

---
+++ ClassLoader.java
@@ -0,0 +1,10 @@
+Explain ClassLoader in Java.
+Answer:
+ClassLoaders load Java classes into JVM at runtime.
+Types:
+
+Bootstrap ClassLoader: Loads core Java classes (rt.jar).
+
+Extension ClassLoader: Loads classes from ext directory.
+
+System/Application ClassLoader: Loads classes from classpath.
