5cf5dbe6bfb49ddf7c492d3bec11dc3efbfaf4f4
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-13 11:17:57 +0530

    Create Thread.java

---
+++ Thread.java
@@ -0,0 +1,8 @@
+What is the difference between wait(), sleep(), and join()?
+Answer:
+
+wait(): Releases lock and waits until notify() or notifyAll() is called.
+
+sleep(): Pauses thread for a given time without releasing lock.
+
+join(): Waits for another thread to finish before continuing.
