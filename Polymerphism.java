5e30317b78948938c7563326022501f7002e0b1d
Sanjana Kumari <sksanjana1502@gmail.com>
2025-08-15 23:50:28 +0530

    Create Polymerphism.java

---
+++ Polymerphism.java
@@ -0,0 +1,17 @@
+class Test 
+{
+    void show(int a,String b)
+    {
+        System.out.println("Hello World");
+    }
+    void show(String b)
+    {
+        System.out.println("I'm Java ");
+    }
+public static void main(String[]args)
+{
+Test t=new Test();
+t.show("Skk");
+t.show(12,"Skk");
+}
+}
