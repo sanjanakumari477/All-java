f73c08a492e6642a85c6848e4bc4f7415461d4ff
Sanjana Kumari <sksanjana1502@gmail.com>
2025-09-23 20:56:42 +0530

    Update checkVowelConso.java

--- checkVowelConso.java
+++ checkVowelConso.java
@@ -7,12 +7,14 @@ import java.util.Scanner;
         char ch= S.next().charAt(0);
           
  	if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
-     	   	{
+     	 
+	       {
             System.out.println("This character is vowel");
                   }
              
               else 
            {
+			   
             System.out.println("This character is consonent");
                    }
                 }
