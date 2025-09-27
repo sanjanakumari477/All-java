bca20dbf9d8f23f5ad29f6d0f189b5178f415080
Sanjana Kumari <sksanjana1502@gmail.com>
2025-09-27 21:12:33 +0530

    Create Difference2.java

---
+++ Difference2.java
@@ -0,0 +1,12 @@
+2. Difference between HashMap and Hashtable
+Ans:
+  
+HashMap:
+Not synchronized → faster.
+Allows one null key and multiple null values.
+Introduced in JDK 1.2.
+
+Hashtable:
+Synchronized → thread-safe, but slower.
+Does not allow null keys or values.
+Legacy class from JDK 1.0.
