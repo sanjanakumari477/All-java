8b93ebd0b5b78c57a1e2e066b07b6733202694b8
Sanjana Kumari <sksanjana1502@gmail.com>
2025-09-27 21:11:34 +0530

    Create Difference.java

---
+++ Difference.java
@@ -0,0 +1,7 @@
+1. Difference between List, Set, and Map
+
+List: Ordered collection, allows duplicates. Indexed access (e.g., ArrayList, LinkedList).
+
+Set: Unordered collection, does not allow duplicates (e.g., HashSet, TreeSet).
+
+Map: Stores key-value pairs. Keys are unique, values can be duplicate (e.g., HashMap, TreeMap).
