a273f525189f1e51c8ce9abe0ae64d0bc82faa48
Sanjana Kumari <sksanjana1502@gmail.com>
2025-09-28 23:18:19 +0530

    Update Exception.java

--- Exception.java
+++ Exception.java
@@ -2,10 +2,12 @@
 public class DivideExample {
     public static void main(String[] args) {
         try {
+            
             int a = 10, b = 0;
             int result = a / b;
             System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
+        }
+        catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero!");
         }
     }
