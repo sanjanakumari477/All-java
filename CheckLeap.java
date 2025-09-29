e28dafe0fd983946734e0949c05bd75b8f26db96
Sanjana Kumari <sksanjana1502@gmail.com>
2025-09-29 21:33:17 +0530

    Update CheckLeap.java

--- CheckLeap.java
+++ CheckLeap.java
@@ -3,15 +3,16 @@ class LeapYear{
  
 public static void main(String[]sk)
 {
-System.out.print("Enter Any Year:");
-Scanner Year=new Scanner(System.in);
-int Y= Year.nextInt();
+	
+	System.out.print("Enter Any Year:");
+	Scanner Year=new Scanner(System.in);
+	int Y= Year.nextInt();
  
 	if((Y%4==0 && Y%100!=0 || Y%400==0))
-	{
+		{
 	System.out.print("This year is leap year.");
-	}
-	else{
+				}
+	else {
 	System.out.print("This year is not leap year.");
 	}
     	}
