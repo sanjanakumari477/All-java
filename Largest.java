4b450bf09498c0e492c5beaf6e7f24fe23d2cecd
Sanjana Kumari <sksanjana1502@gmail.com>
2025-10-05 19:45:37 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
