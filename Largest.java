0e536166b491f2b7a8f48b79a0a9477a708aed2e
Sanjana Kumari <sksanjana1502@gmail.com>
2025-10-05 19:45:25 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -2,20 +2,15 @@ import java.util.Scanner;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        // Create Scanner object for user input
+        
         Scanner sc = new Scanner(System.in);
-
-        // Take input from the user
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
-
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
-
         System.out.print("Enter third number: ");
         int c = sc.nextInt();
-
-        // Logic to find the largest number
+        
         int largest;
         if (a >= b && a >= c) {
             largest = a;
@@ -24,11 +19,9 @@ public class LargestNumber {
         } else {
             largest = c;
         }
-
-        // Output the result
+   
         System.out.println("The largest number is: " + largest);
 
-        // Close the scanner
         sc.close();
     }
 }
