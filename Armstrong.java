872948c8424385816001f43773b01e622b5bab0a
Sanjana Kumari <sksanjana1502@gmail.com>
2026-04-11 22:42:30 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -4,8 +4,6 @@ public class ArmstrongNumber
 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-
         System.out.print("Enter a number: ");
         int number = input.nextInt();
 
