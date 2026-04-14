7c80d7de96f85394732db845b2746ae8d29a1d64
Sanjana Kumari <sksanjana1502@gmail.com>
2026-04-14 22:42:05 +0530

    Update AlbhaCheck.java

--- AlbhaCheck.java
+++ AlbhaCheck.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 	class Alphabet{
         public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);	
 	System.out.print("enter any character=");
 	
