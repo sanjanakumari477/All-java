e4e1d86e273c751b94e814db8be1d9894d6268fb
Sanjana Kumari <sksanjana1502@gmail.com>
2026-04-17 22:02:42 +0530

    Update Arith_problem.java

--- Arith_problem.java
+++ Arith_problem.java
@@ -6,7 +6,6 @@ public class ArithmeticOperations
         Scanner input = new Scanner(System.in);
         System.out.print("Enter first number: ");
         double num1 = input.nextDouble();
-
         System.out.print("Enter second number: ");
         double num2 = input.nextDouble();
 
